8e708bd7b321c8934b4edacb69a342075e47eed3
zeze <ylliwq@gapp.nthu.edu.tw>
2021-01-25 15:56:36 +0800

    分離程式碼

--- Day1-2.py
+++ Day1-2.py
@@ -5,6 +5,4 @@ print(a+b)
 # , can be used to seperate different strings and values in print function
 print ("a+b =",a+b)
 
-apple=int(input("please input how many apples?"))
-student=int(input("please input how many students?"))
-print("Each student can get",apple//student,", and basket remained",apple%student)
+
---
+++ Day1-3.py
@@ -0,0 +1,3 @@
+apple=int(input("please input how many apples?"))
+student=int(input("please input how many students?"))
+print("Each student can get",apple//student,", and basket remained",apple%student)
